922456fa671fef4dbcaa6937a9ef91da71955f39
challa chinnaobulesh <challaobulesh3@gmail.com>
2026-08-23 21:39:50 +0530

    Add HackerRank Diagonal Difference solution

---
+++ HackerRank/DiagonalDifference/DiagonalDifference.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+class Result {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int primarySum = 0;
+        int secondarySum = 0;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            primarySum += arr.get(i).get(i);
+            secondarySum += arr.get(i).get(n - 1 - i);
+        }
+
+        return Math.abs(primarySum - secondarySum);
+    }
+}
