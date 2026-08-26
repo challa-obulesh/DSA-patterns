6aea3d43125403d41c811587b746ab3eedf2e3b8
challa chinnaobulesh <challaobulesh3@gmail.com>
2026-08-26 15:26:04 +0530

    Update 3Sum with optimal O(n^2) solution

--- LeetCode/Two-Pointers/ThreeSum/ThreeSum.java
+++ LeetCode/Two-Pointers/ThreeSum/ThreeSum.java
@@ -7,6 +7,7 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
@@ -30,6 +31,7 @@ class Solution {
                     while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
+
                 } else if (sum < 0) {
                     left++;
                 } else {
